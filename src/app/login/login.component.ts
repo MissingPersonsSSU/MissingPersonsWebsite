575c2d61e2360d54c62e4bf11b5af732e646f98c
Matthew Booysen <MatthewBoo@awards.co.za>
2020-03-12 11:38:24 +0200

    removed code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   errorMessage = '';
-  googlePath = 'src\assets\Social-Media-Icons\Google.png';
 
   constructor() { }
 
